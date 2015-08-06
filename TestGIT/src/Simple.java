79cac41de1769e99c65bb7089c46c3871d5065d8
sergei <serge.ingber@checkmarx.com>
2015-08-06 13:28:13 +0300

    Added additional outputs through a new method: DisplayMessages

--- TestGIT/src/Simple.java
+++ TestGIT/src/Simple.java
@@ -4,6 +4,13 @@ public class Simple {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		displayMessages();
+	}
+	
+	private static void displayMessages()
+	{
+		System.out.println("What a wonderful day!");
+		System.out.println("See you later!");
 	}
 
 }
