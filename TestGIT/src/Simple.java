935a401c5ac7e9373379a3e165785ef704407894
sergei <serge.ingber@checkmarx.com>
2015-08-09 10:47:21 +0300

    Pass parameters to DisplayMessage

--- TestGIT/src/Simple.java
+++ TestGIT/src/Simple.java
@@ -6,10 +6,10 @@ public class Simple {
 		System.out.println("Hello World");
 		String [] s = args;
 		
-		displayMessages();
+		displayMessages(s);
 	}
 	
-	private static void displayMessages()
+	private static void displayMessages(String [] s)
 	{
 		System.out.println("What a wonderful day!");
 		System.out.println("See you later!");
