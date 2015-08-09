d233205fd37654f8a56ff48ee7e435f785205f2e
sergei <serge.ingber@checkmarx.com>
2015-08-09 10:41:53 +0300

    Get args parameter

--- TestGIT/src/Simple.java
+++ TestGIT/src/Simple.java
@@ -4,6 +4,8 @@ public class Simple {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		String [] s = args;
+		
 		displayMessages();
 	}
 	
